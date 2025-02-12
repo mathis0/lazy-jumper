8d0f60b03fb6ef01d67e4646eec460fff89706e6
Mohammadhosein.a <Mohammadhosein.a@behinrahkar.com>
2025-02-12 21:48:16 +0330

    changing keybinding

--- lua/line_jumper.lua
+++ lua/line_jumper.lua
@@ -9,15 +9,15 @@ M.original_hls = {}
 
 -- Key mappings for the custom mode
 local keymap = {
-	q = "1",
-	w = "2",
-	e = "3",
-	r = "4",
-	t = "5",
-	y = "6",
-	u = "7",
-	i = "8",
-	o = "9",
+	a = "1",
+	s = "2",
+	d = "3",
+	f = "4",
+	g = "5",
+	h = "6",
+	j = "7",
+	k = "8",
+	l = "9",
 	p = "0",
 }
 
